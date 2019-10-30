245c13872b33100c15546a64896df5c43f32d01d
Sandy Souza Santos <sandysouzasantos@localhost.localdomain>
2019-10-30 15:50:27 -0300

    Added cliente.model

---
+++ models/cliente.model.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const clienteSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+    },
+    rua: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+    },
+    numero: {
+        type: Number,
+        required: true,
+    },
+    bairro: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+    },
+    cidade: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+    },
+    estado: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+    },
+    telefone: {
+        type: String,
+        required: true,
+        maxlength: 11,
+    },
+    nascimento: {
+        type: Date,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+    },
+});
+
+module.exports.Cliente = mongoose.model('Cliente', clienteSchema);
